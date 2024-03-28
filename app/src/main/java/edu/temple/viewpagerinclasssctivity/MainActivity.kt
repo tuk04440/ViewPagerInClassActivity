4d8a0dc48f0ca3f07089a5f0e7485c784ff3ef2b
Allen <tuk04440@temple.edu>
2024-03-28 11:38:07 -0400

    Created pager for fragment

--- app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
+++ app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.temple.viewpagerinclasssctivity
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,10 +9,19 @@ import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val viewPager = findViewById<ViewPager2>(R.id.viewPager)
+
+        viewPager.adapter = object: FragmentStateAdapter (this) {
+            override fun getItemCount() = 10
+
+            override fun createFragment(position: Int) = TextFragment.newInstance((position + 1).toString())
+
+        }
     }
 
 }
